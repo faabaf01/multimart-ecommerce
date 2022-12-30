f4e5d7d5328ec01fe0c23ca8eb6add412a533547
faabaf01 <farasya15@gmail.com>
2022-12-31 01:31:32 +0800

    added icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return <h1>I am here</h1>;
+  return <Layout />;
 }
 
 export default App;
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
 import Cart from "../pages/Cart";
@@ -10,6 +10,7 @@ import Signup from "../pages/Signup";
 const Routers = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="home" />} />
       <Route path="home" element={<Home />} />
       <Route path="shop" element={<Shop />} />
       <Route path="shop/:id" element={<ProductDetails />} />
