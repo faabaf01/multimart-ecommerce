70e4995202fc0aa60bc59944196818f0ed35118b
faabaf01 <farasya15@gmail.com>
2023-01-19 14:27:26 +0800

    modified header part

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./header.css";
 
+import { motion } from "framer-motion";
+
 import logo from "../../assets/images/eco-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 
 import { Container, Row } from "reactstrap";
+
+const nav__links = [
+  {
+    path: "home",
+    display: "Home",
+  },
+  {
+    path: "shop",
+    display: "Shop",
+  },
+  {
+    path: "cart",
+    display: "Cart",
+  },
+];
 
 const Header = () => {
   return (
@@ -18,32 +35,37 @@ const Header = () => {
               <img src={logo} alt="logo" />
               <div>
                 <h1>Multimart</h1>
-                <p>Since 1995</p>
+                {/* <p>Since 1995</p> */}
               </div>
             </div>
             <div className="navigation">
               <ul className="menu">
-                <li className="nav__item">
-                  <NavLink to="home">Home</NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink to="shop">Shop</NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink to="cart">Cart</NavLink>
-                </li>
+                {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "nav__active" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="nav__icons">
               <span className="fav__icon">
                 <i class="ri-heart-line"></i>
+                <span className="badge">1</span>
               </span>
 
               <span className="cart__icon">
                 <i class="ri-shopping-bag-line"></i>
+                <span className="badge">1</span>
               </span>
               <span>
-                <img src={userIcon} alt="" />
+                <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
               </span>
             </div>
             <div className="mobile__menu">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
 
 const Home = () => {
-  return <div>Home</div>;
+  const year = new Date().getFullYear();
+
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending product in {year}</p>
+                <h2>Make your interior more minimalistic & modern </h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Earum quibusdam dicta consequatur id, sequi molestiae sint
+                  minima placeat, voluptatem labore velit repellat quod beatae
+                  commodi. Iusto, animi. Mollitia, earum possimus?
+                </p>
+                <button className="buy__btn">SHOP NOW</button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
